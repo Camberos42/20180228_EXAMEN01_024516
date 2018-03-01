cf85c2b903a711b0eeba89145b2ccf0e36116f40
Camberos42 <35781327+Camberos42@users.noreply.github.com>
2018-02-28 22:02:49 -0800

    Update Banco.java

--- Banco.java
+++ Banco.java
@@ -2,6 +2,15 @@
 public abstract class Banco {
 	
 	private double saldo;
+	private Protocolo p;
+	
+	public void setValidacion(Protocolo p){
+		this.p = p;
+	}
+	
+	public Protocolo getValidacion(){
+		return this.p;
+	}
 	
 	public void transferencia(){
 		System.out.println("Transfiriendo");
