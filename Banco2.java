158ab00e13c7f775a44355e87dc53efc59b62723
Camberos42 <35781327+Camberos42@users.noreply.github.com>
2018-02-28 22:03:37 -0800

    Update Banco2.java

--- Banco2.java
+++ Banco2.java
@@ -1,4 +1,12 @@
 
 public class Banco2 {
+  double cuenta_origen;
+	double cuenta_destino;
+	double monto;
+	
+	public void transferencia(){
+		System.out.println("Transfiriendo desde el banco 2");
+		
+	}
 
 }
